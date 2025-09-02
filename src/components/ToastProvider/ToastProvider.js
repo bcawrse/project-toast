0707d37759a065d5183c8d49b0d2a4b7762951a8
Ben Cawrse <bcawrse@gmail.com>
2025-09-01 22:46:18 -0500

    Exercise 5.1

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,20 @@ export const ToastContext = React.createContext(null);
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    function handleEscapeKeydown(e) {
+      if (e.key === "Escape") {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener("keydown", handleEscapeKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleEscapeKeydown);
+    };
+  }, []);
+
   function addToast({ variant, message }) {
     if (message == null || message === "") return;
 
